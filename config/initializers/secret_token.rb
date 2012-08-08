e29237999bd0dc661760f78446bd76c535e79ca0
Fatman <fanan1985@gmail.com>
2012-08-08 23:45:38 +0800

    skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineVideoDownloader::Application.config.secret_token = 'ecc6e60346958f462b55505fb864e55edb689cadb9d8920f0758fb987b64219a257f42257a16b32e17a06f9e0de38cc1c4c01b7e0627bd6e46a213cc27e66ea7'
